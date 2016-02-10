0a5c1ecabaf66a1e8ba3e18057c3d88fcafcfd1c
mohsaad <msaad96@gmail.com>
2016-02-10 12:41:44 -0600

    added feature plotting

---
+++ kml_refresh/feat_refresh/plot_real_time.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# Mohammad Saad
+# 2/3/2016
+# Plots points on KML in
+# Real time
+
+import os
+import random
+import xml.etree.ElementTree as ET
+import utm
+
+class RealTimePlotter:
+
+	def __init__(self, header_file, output_file):
+
+		self.header = header_file
+		self.output_file = output_file
+
+		self.feat_dict = {}
+
+		self.dom =ET.parse(self.header)
+		self.root = self.dom.getroot()
+
+		self.document = self.root.findall(append_gis_str('Document'))[0]
+
+		folder_top = self.document.findall(append_gis_str('Folder'))[0]
+		folder = folder_top.findall(append_gis_str('Folder'))[0]
+
+		self.actual_points = ET.SubElement(folder, append_gis_str('Folder'))
+		name_points = ET.SubElement(self.actual_points, append_gis_str('Name'))
+		name_points.text = "Points"
+
+		line_placemarks = ET.SubElement(folder, append_gis_str('Placemark'))
+		name = ET.SubElement(line_placemarks, append_gis_str('Name'))
+		name.text = "Path"
+
+		style_url = ET.SubElement(line_placemarks, append_gis_str('styleUrl'))
+		style_url.text = '#lineStyle'
+
+		line_string = ET.SubElement(line_placemarks, append_gis_str('lineString'))
+
+		tesselate = ET.SubElement(line_string, append_gis_str('tessellate'))
+		tesselate.text = "1"
+
+		self.coordinates = ET.SubElement(line_string, append_gis_str('coordinates'))
+		self.coordinates.text = ''
+		# test
+		self.dom.write(self.output_file)
+
+
+	def create_point_placemark(self, coords):
+		# add to points folder
+		pmark = ET.SubElement(self.actual_points, append_gis_str('Placemark'))
+			
+		styleurl = ET.SubElement(pmark, append_gis_str('styleUrl'))
+		styleurl.text = "#track"
+
+		lookat = ET.SubElement(pmark, append_gis_str('LookAt'))
+		lat = ET.SubElement(lookat, append_gis_str('latitude'))
+		lat.text = str(coords[0])
+
+		longc = ET.SubElement(lookat, append_gis_str('longitude'))
+		longc.text = str(coords[1])
+
+		tilt = ET.SubElement(lookat, append_gis_str('tilt'))
+		tilt.text = "66"
+
+		point = ET.SubElement(pmark, append_gis_str('Point'))
+		coordtag = ET.SubElement(point, append_gis_str('coordinates'))
+		coordtag.text = '{0},{1}'.format(coords[1], coords[0])
+
+		self.coordinates.text += '{0},{1}\n'.format(coords[1], coords[0])
+
+	def create_feature_placemark(self, coords, count):
+		# add to points folder
+
+		if count in self.feat_dict:
+			self.feat_dict[count][0].text = str(coords[0])
+			self.feat_dict[count][1].text = str(coords[1])
+			self.feat_dict[count][2].text = '{0},{1}'.format(coords[1], coords[0])
+			return
+
+		pmark = ET.SubElement(self.actual_points, append_gis_str('Placemark'))
+			
+		styleurl = ET.SubElement(pmark, append_gis_str('styleUrl'))
+		styleurl.text = "#feats"
+
+		lookat = ET.SubElement(pmark, append_gis_str('LookAt'))
+		lat = ET.SubElement(lookat, append_gis_str('latitude'))
+		lat.text = str(coords[0])
+
+		longc = ET.SubElement(lookat, append_gis_str('longitude'))
+		longc.text = str(coords[1])
+
+		tilt = ET.SubElement(lookat, append_gis_str('tilt'))
+		tilt.text = "66"
+
+		point = ET.SubElement(pmark, append_gis_str('Point'))
+		coordtag = ET.SubElement(point, append_gis_str('coordinates'))
+		coordtag.text = '{0},{1}'.format(coords[1], coords[0])
+
+		self.feat_dict[count] = (lat,longc, coordtag)
+
+
+	def read_and_write_to_output(self, zone):
+		
+		# loop
+		total_count = 0
+		count = 0
+		line = ''
+		while line is not "END":
+			line = raw_input()
+			total_count += 1
+			# if line is end, stop
+			if line == "END":
+				print "End of Stream"
+				break
+
+			# split up line
+			line_arr = line.split(',')
+			
+
+			# plot actual path 
+			if len(line_arr) == 10:
+				coords = utm.to_latlon(float(line_arr[2]), float(line_arr[1]), zone, 'T')
+				self.create_point_placemark(coords)
+			# plot features
+			elif len(line_arr) == 4:
+				count = int(line_arr[0])
+				# convert to latlon
+				coords = utm.to_latlon(float(line_arr[2]), float(line_arr[1]), zone, 'T')
+				self.create_feature_placemark(coords, count)
+
+			# otherwise continue
+			else:
+				pass
+
+			# write to file every 100 samples
+			if line_arr[0] == '\r' and total_count % 1000 == 0:
+				self.dom.write(self.output_file)
+
+
+
+def append_gis_str(string):
+	return '{http://www.opengis.net/kml/2.2}'+ string
+
+
+def main():
+	rtp = RealTimePlotter('header.kml', 'real_time_output.kml')
+	rtp.read_and_write_to_output(16)
+
+if __name__ == '__main__':
+	main()
