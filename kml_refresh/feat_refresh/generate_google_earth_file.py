36e2eea112274185f6ea85afa181bb1ab951051f
mohsaad <msaad96@gmail.com>
2016-03-13 23:32:49 -0500

    finished mapping, time to clean up

---
+++ kml_refresh/feat_refresh/generate_google_earth_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# Mohammad Saad
+# 3/12/2016
+# generate_google_earth_file.py
+# Generates a Google Earth KML to put in 
+# So that we can put in features
+
+import xml.etree.ElementTree as ET
